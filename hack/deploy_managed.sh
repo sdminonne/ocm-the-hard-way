e9320c2a6eb4b55c77d3fb1567deebcd2f72d327
Salvatore Dario Minonne <sminonne@redhat.com>
2022-08-17 13:32:28 +0200

    to refresh ocm the hard way

--- hack/deploy_managed.sh
+++ hack/deploy_managed.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-source ./hack/common.sh
+ROOTDIR=$(git rev-parse --show-toplevel)
+source "${ROOTDIR}/hack/common.sh"
 
 ################################################################################
 # Help: displays usage
@@ -10,21 +11,21 @@ Help()
  # Display Help
  echo "$0 deploys OCM managed cluster "
  echo
- echo "Syntax: deploy_hub [ -b|e|h|n|p ]" 
+ echo "Syntax: deploy_hub [ -b|e|h|n|p ]"
  echo "options:"
- echo "-b <hub name> Specify the name of the cluster, Default: ${DEFAULT_HUBNAME}"
- echo "-e <docker> Specify the container engine. Default : ${DEFAULT_CONTAINER_ENGINE}"
+ echo "-b <hub name> Specify the name of the cluster, Default: \"${DEFAULT_HUBNAME}\""
+ echo "-e <docker> Specify the container engine. Default : \"${DEFAULT_CONTAINER_ENGINE}\""
  echo "-h Print this help."
- echo "-n <cluster name> Specify the name of the cluster, Default: ${DEFAULT_MANAGEDNAME}"
- echo "-p <kind|minikube> Specify the cluster provider: kind or minikube. Default ${DEFAULT_CLUSTER_PROVIDER}"
+ echo "-n <cluster name> Specify the name of the cluster, Default: \"${DEFAULT_MANAGEDNAME}\""
+ echo "-p <kind|minikube> Specify the cluster provider: kind or minikube. Default: \"${DEFAULT_CLUSTER_PROVIDER}\""
  echo
  echo "Exmples:"
  echo "./hack/deploy_managed.sh -b hub -n cluster-foo -p minikube"
 }
 
 
-LOCAL_CONTAINER_ENGINE=${DEFAULT_CONTAINER_ENGINE}
-LOCAL_CLUSTER_PROVIDER=${DEFAULT_CLUSTER_PROVIDER}
+CONTAINER_ENGINE=${DEFAULT_CONTAINER_ENGINE}
+CLUSTER_PROVIDER=${DEFAULT_CLUSTER_PROVIDER}
 HUBNAME=${DEFAULT_HUBNAME}
 MANAGEDNAME=${DEFAULT_MANAGEDNAME}
 
@@ -36,14 +37,14 @@ while getopts "b:e:hn:p:" arg; do
  case $arg in
      b) HUBNAME=${OPTARG}
 	;;
-     e) LOCAL_CONTAINER_ENGINE=${OPTARG}
+     e) CONTAINER_ENGINE=${OPTARG}
 	;;
      h)	 Help
 	 exit
 	 ;;
      n) MANAGEDNAME=${OPTARG}
 	;;
-     p) LOCAL_CLUSTER_PROVIDER=${OPTARG}
+     p) CLUSTER_PROVIDER=${OPTARG}
 	;;
      *)  Help
 	 exit
@@ -54,100 +55,49 @@ shift $((OPTIND-1))
 
 echo_green "Managed cluster name -> ${MANAGEDNAME}"
 echo_green "Hub cluster name     -> ${HUBNAME}"
-echo_green "Container engine     -> ${LOCAL_CONTAINER_ENGINE}"
-echo_green "Cluster provider     -> ${LOCAL_CLUSTER_PROVIDER=}"
+echo_green "Container engine     -> ${CONTAINER_ENGINE}"
+echo_green "Cluster provider     -> ${CLUSTER_PROVIDER=}"
+
+build_images
+
 
 #TODO: we should check hubname is a real working cluster
-
 create_cluster ${MANAGEDNAME}
 
 deploy_images_to_cluster ${MANAGEDNAME}
 
+IMAGE_REGISTRY="localhost:5000"
+IMAGE_TAG="latest"
 MANAGEDCONTEXT=$(get_client_context_from_cluster_name ${MANAGEDNAME})
 HUBCONTEXT=$(get_client_context_from_cluster_name ${HUBNAME})
 
-kubectl --context=${MANAGEDCONTEXT} apply -f  ./artifacts/managed/crds/
+echo_green "Managed context -> ${MANAGEDCONTEXT}"
+echo_green "Hub context     -> ${HUBCONTEXT}"
 
-#TODO checks CRDs
-#appliedmanifestworks.work.open-cluster-management.io
-#klusterlets.operator.open-cluster-management.io
+kubectl --context hub config view --flatten --minify > hub-kubeconfig
 
+export MANAGED_CLUSTER_NAME=${MANAGEDNAME}
+export MANAGEDCONTEXT
+export HUBCONTEXT
 
-kubectl --context=${MANAGEDCONTEXT} create ns open-cluster-management
+cd repos/registration-operator/deploy/klusterlet/config && kustomize edit set image localhost:5000/open-cluster-management/registration-operator:latest && cd - || exit
+cd repos/registration-operator && make deploy-spoke && cd - || exit
+
 wait_until "namespace_active ${MANAGEDCONTEXT} open-cluster-management" 5 60
-
-kubectl --context=${MANAGEDCONTEXT} apply -f artifacts/managed/open-cluster-management/klusterlet-sa.yaml
-kubectl --context=${MANAGEDCONTEXT} apply -f artifacts/managed/clusterroles/open-cluster-management-klusterlet.yaml 
-kubectl --context=${MANAGEDCONTEXT} apply -f artifacts/managed/clusterrolebindings/open-cluster-management-klusterlet.yaml
-kubectl --context=${MANAGEDCONTEXT} apply -f artifacts/managed/open-cluster-management/klusterlet-deployment.yaml
 wait_until "deployment_up_and_running ${MANAGEDCONTEXT} open-cluster-management klusterlet" 10 60
 
-
-
-kubectl --context=${MANAGEDCONTEXT} create ns open-cluster-management-agent
 wait_until "namespace_active ${MANAGEDCONTEXT} open-cluster-management-agent" 5 60
-
-#tmpkubeconfig=$(mktemp)
-#kubectl --context=${HUBCONTEXT} config view --flatten --minify > ${tmpkubeconfig}
-tmpkubeconfig=$(generate_kubeconfig_for_cluster ${HUBNAME})
-echo_green "Generated kubeconfig for secret ${tmpkubeconfig}"
-kubectl --context=${MANAGEDCONTEXT} create secret generic bootstrap-hub-kubeconfig --from-file=kubeconfig="${tmpkubeconfig}" -n open-cluster-management-agent
-
-
-kubectl --context=${MANAGEDCONTEXT} apply -f  artifacts/managed/open-cluster-management-agent/klusterlet-registration-sa.yaml
-kubectl --context=${MANAGEDCONTEXT} apply -f  artifacts/managed/clusterroles/open-cluster-management-agent-klusterlet-registration.yaml
-kubectl --context=${MANAGEDCONTEXT} apply -f artifacts/managed/clusterrolebindings/open-cluster-management-agent-klusterlet-registration.yaml
-
-
-cat artifacts/managed/open-cluster-management-agent/klusterlet-registration-agent-deployment-template.yaml | \
-    sed "s/MANAGEDCLUSTERNAME/${MANAGEDNAME}/g" | \
-    kubectl  --context=${MANAGEDCONTEXT} apply -f -
 wait_until "deployment_up_and_running ${MANAGEDCONTEXT} open-cluster-management-agent klusterlet-registration-agent" 10 60
-
-
-kubectl --context=${HUBCONTEXT} get managedclusters
-
-
-cat <<EOF | kubectl --context=${MANAGEDCONTEXT} apply -f -
-apiVersion: operator.open-cluster-management.io/v1
-kind: Klusterlet
-metadata:
-  name: klusterlet
-spec:
-  registrationImagePullSpec: localhost:5000/open-cluster-management/registration:latest
-  workImagePullSpec: localhost:5000/open-cluster-management/work:latest
-  clusterName: ${MANAGEDNAME}
-  namespace: open-cluster-management-agent
-  externalServerURLs:
-  - url: https://localhost
-EOF
-
 
 wait_until "csr_submitted ${HUBCONTEXT} ${MANAGEDNAME}" 5 60
 
 csrname=$(kubectl --context=${HUBCONTEXT} get csr -o=jsonpath="{.items[?(@.metadata.generateName=='${MANAGEDNAME}-')].metadata.name}")
 kubectl --context=${HUBCONTEXT} certificate approve  $csrname
+kubectl  --context=${HUBCONTEXT} patch managedcluster ${MANAGEDNAME} -p='{"spec":{"hubAcceptsClient":true}}' --type=merge
 
-# TOO replace with wait_until
+cd repos/work && make deploy && cd - #|| { echo_red >&2 "Unable to deploy work. Aborting"; exit 1; }
 
-
-kubectl --context=${HUBCONTEXT} get managedclusters
-
-
-kubectl --context=${HUBCONTEXT} patch managedcluster  ${MANAGEDNAME} -p='{"spec":{"hubAcceptsClient":true}}' --type=merge
-
-kubectl --context=${HUBCONTEXT} get managedclusters
-
-kubectl --context=${MANAGEDCONTEXT} apply -f  artifacts/managed/open-cluster-management-agent/klusterlet-work-sa.yaml
-kubectl --context=${MANAGEDCONTEXT} apply -f artifacts/managed/clusterroles/open-cluster-management-agent-klusterlet-work.yaml
-kubectl --context=${MANAGEDCONTEXT} apply -f artifacts/managed/clusterrolebindings/open-cluster-management-agent-klusterlet-work.yaml
-
-
-cat artifacts/managed/open-cluster-management-agent/klusterlet-work-agent-deployment-template.yaml | \
-    sed "s/MANAGEDCLUSTERNAME/${MANAGEDNAME}/g" | \
-    kubectl  --context=${MANAGEDCONTEXT} apply -f -
-
-wait_until "deployment_up_and_running ${MANAGEDCONTEXT} open-cluster-management-agent klusterlet-work-agent" 5 30
+wait_until "deployment_up_and_running ${MANAGEDCONTEXT} open-cluster-management-agent klusterlet-work-agent" 10 60
 
 cat <<EOF | kubectl --context=${HUBCONTEXT} apply -f -
 apiVersion: work.open-cluster-management.io/v1
@@ -172,5 +122,4 @@ spec:
 EOF
 
 wait_until "pod_up_and_running ${MANAGEDCONTEXT} default hello" 10 120
-
 kubectl --context=${MANAGEDCONTEXT} -n default logs hello
